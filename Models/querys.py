5e4acabad45b8983909830cbadad1e4c7538c741
Noé Zúñiga <noe.zm2000@gmail.com>
2023-02-14 20:24:30 -0600

    Se añade función para consultar campos de una tabla

--- Models/querys.py
+++ Models/querys.py
@@ -1,15 +1,24 @@
 from database import database_connection as db
 
-
-# database = db(host="187.131.147.109",
-#     user="Aurelio",
-#     password="RG980320",
-#     database="Parqueadero1")
 data_base = db(
                 host="localhost",
                 user="root",
                 password="",
                 database="db")
+
+
+class Querys:
+     def __init__():
+          pass
+     def obtener_campos_tabla(self, campo):
+          query = "PRAGMA table_info({self.campo})"
+          db.execute_query(self, query, campo)
+
+# database = db(host="187.131.147.109",
+#     user="Aurelio",
+#     password="RG980320",
+#     database="Parqueadero1")
+
 
 
 a = data_base.execute_query("SELECT * FROM Usuarios")
