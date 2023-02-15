15eb54042545b697c2f489c3f30dd803b3ee3ca4
Noé Zúñiga <noe.zm2000@gmail.com>
2023-02-14 20:24:05 -0600

    Se añaden frame de campos y tabla al panel

--- Views/view_panel.py
+++ Views/view_panel.py
@@ -1,57 +1,103 @@
 import tkinter as tk
 
-from datetime import datetime, date, time, timedelta
-import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox as mb
-from tkinter import scrolledtext as st
-from tkinter import font
-from tkinter import *
-from tkinter import simpledialog
-class FormularioOperacion:
+from tkinter import messagebox
+
+class Aplicacion:
     def __init__(self):
-        #creamos un objeto que esta en el archivo operacion dentro la clase Operacion
-        self.ventana1=tk.Tk()
-        self.ventana1.title("Generar reportes")
-        self.cuaderno1 = ttk.Notebook(self.ventana1)
-        self.cuaderno1.config(cursor="")         # Tipo de cursor
-        self.ExpedirRfid()
+        self.panel=tk.Tk()
+        self.panel.geometry("800x300")
+        self.panel.title("Panel de administración")
+
+        self.tabla()
+        self.panel.columnconfigure(0, weight=1)  # Configurar la columna principal del panel
+
+        self.campos_consulta()
 
 
-        self.cuaderno1.grid(column=0, row=0, padx=5, pady=5)
-        self.ventana1.mainloop()
-        ####################Inicia Pagina1###################
+        self.panel.mainloop()
 
 
-    def ExpedirRfid(self):
-        self.pagina1 = ttk.Frame(self.cuaderno1)
-        self.cuaderno1.add(self.pagina1, text="Expedir Boleto")
+    def tabla(self):
+        seccion_tabla = ttk.LabelFrame(self.panel, text="Tabla")
+        seccion_tabla.grid(row=0, column=0)
+
+        # Crea un Treeview con 3 columnas
+        tabla = ttk.Treeview(seccion_tabla, columns=("Nombre", "Apellido", "Edad"))
+
+        # Define los encabezados de columna
+        tabla.heading("#0", text="ID")
+        tabla.heading("Nombre", text="Nombre")
+        tabla.heading("Apellido", text="Apellido")
+        tabla.heading("Edad", text="Edad")
+
+        # Inserta datos de ejemplo
+        tabla.insert("", "end", text="1", values=("Juan", "Pérez", 25))
+        tabla.insert("", "end", text="2", values=("María", "González", 30))
+        tabla.insert("", "end", text="3", values=("Pedro", "García", 35))
+        tabla.insert("", "end", text="1", values=("Juan", "Pérez", 25))
+
+        # Ajusta el ancho de cada columna
+        tabla.column("#0", width=50, stretch = True)
+        tabla.column("Nombre", width=100)
+        tabla.column("Apellido", width=100)
+        tabla.column("Edad", width=50)
+
+        # Crea un Scrollbar vertical y lo asocia con el Treeview
+        scrollbar = ttk.Scrollbar(seccion_tabla, orient="vertical", command=tabla.yview)
+        tabla.configure(yscroll=scrollbar.set)
+        scrollbar.pack(side="right", fill="y")
+
+        # Empaqueta el Treeview en la ventana
+        tabla.pack(anchor='n', fill="both", expand=True)
+
+    def campos_consulta(self):
+        # Crear un seccion_campos_consulta para los campos de texto
+        seccion_campos_consulta = ttk.LabelFrame(self.panel, text="Consulta de datos", padding=10)
+        seccion_campos_consulta.grid(row=0, column=1, sticky='n')
+        
+        # Crear los campos de texto
+        self.campo_texto_corte = tk.Entry(seccion_campos_consulta)
+        self.campo_texto_fecha_inicio = tk.Entry(seccion_campos_consulta)
+        self.campo_texto_fecha_fin = tk.Entry(seccion_campos_consulta)
+        self.campo_texto_folio = tk.Entry(seccion_campos_consulta)
+        
+        # Empaqueta los campos de texto en el seccion_campos_consulta
+        self.campo_texto_corte.grid(row=0, column=0)
+        self.campo_texto_fecha_inicio.grid(row=1, column=0)
+        self.campo_texto_fecha_fin.grid(row=2, column=0)
+        self.campo_texto_folio.grid(row=3, column=0)
+        
+
+        # Crear las leyendas para los campos de texto
+        leyenda_corte = ttk.Label(seccion_campos_consulta, text="Corte:")
+        leyenda_fecha_inicio = ttk.Label(seccion_campos_consulta, text="Fecha inicio:")
+        leyenda_fecha_fin = ttk.Label(seccion_campos_consulta, text="Fecha fin:")
+        leyenda_folio = ttk.Label(seccion_campos_consulta, text="Folio:")
+
+        # Empaqueta las leyendas y los campos de texto en el seccion_campos_consulta
+        leyenda_corte.grid(row=0, column=0, padx=5, pady=5, sticky="W")
+        self.campo_texto_corte.grid(row=0, column=1, padx=5, pady=5)
+        leyenda_fecha_inicio.grid(row=1, column=0, padx=5, pady=5, sticky="W")
+        self.campo_texto_fecha_inicio.grid(row=1, column=1, padx=5, pady=5)
+        leyenda_fecha_fin.grid(row=2, column=0, padx=5, pady=5, sticky="W")
+        self.campo_texto_fecha_fin.grid(row=2, column=1, padx=5, pady=5)
+        leyenda_folio.grid(row=3, column=0, padx=5, pady=5, sticky="W")
+        self.campo_texto_folio.grid(row=3, column=1, padx=5, pady=5)
 
 
-        #enmarca los controles LabelFrame
-        self.labelframe1=ttk.LabelFrame(self.pagina1, text="Dar Entrada")
-        self.labelframe1.grid(column=1, row=0, padx=0, pady=0)
-        self.MaxId=tk.StringVar()
-        self.entryMaxId=ttk.Entry(self.labelframe1, width=10, textvariable=self.MaxId, state="readonly")
-        self.entryMaxId.grid(column=1, row=0, padx=4, pady=4)
-        self.lbltitulo=ttk.Label(self.labelframe1, text="FOLIO")
-        self.lbltitulo.grid(column=0, row=0, padx=0, pady=0)
 
 
-        #####tomar placas del auto
-        self.Placa=tk.StringVar()
-        self.entryPlaca=tk.Entry(self.labelframe1, width=10, textvariable=self.Placa)
-        self.entryPlaca.grid(column=1, row=1, padx=4, pady=4)
-        self.lblPlaca=ttk.Label(self.labelframe1, text="COLOCAR PLACAS")
-        self.lblPlaca.grid(column=0, row=1, padx=0, pady=0)
+        # Crea un botón y lo empaqueta en el seccion_campos_consulta
+        boton_consulta = tk.Button(seccion_campos_consulta, text="Consulta")
+        boton_consulta.grid(row=4, column=0)
 
+        boton_generar_reporte = tk.Button(seccion_campos_consulta, text="Generar reporte")
+        boton_generar_reporte.grid(row=5, column=0)
 
+    def mostrar_mensaje(self):
+        """Muestra un mensaje en una ventana emergente."""
+        mensaje = tk.messagebox.showinfo("Mensaje", "Boton presionado")
 
-
-
-
-        #Boton - Salir del programa
-        self.boton2=tk.Button(self.pagina1, text="Salir del programa", command=quit, width=15, height=1, anchor="center")
-        self.boton2.grid(column=0, row=0, padx=4, pady=4)
-
-aplicacion1=FormularioOperacion()
+if __name__ == "__main__":
+    app = Aplicacion()
